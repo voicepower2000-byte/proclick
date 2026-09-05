75b87c089df0f1bb0dbced1f27a631d94f7ba06e
voicepower2000-byte <voicepower2000@gmail.com>
2026-09-05 04:15:07 +0300

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,14 +1,15 @@
-const CACHE_NAME = 'proclick-v1';
-const urlsToCache = ['/', '/index.html', '/manifest.json'];
+const CACHE_NAME = 'proclick-v2';
 
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
-  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => response || fetch(event.request))
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
