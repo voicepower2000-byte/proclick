a182f6bb4d49fa4ed7dd9e668a4c865905f95743
voicepower2000-byte <voicepower2000@gmail.com>
2026-09-05 04:44:08 +0300

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,15 +1,40 @@
-const CACHE_NAME = 'proclick-v2';
+const CACHE_NAME = 'proclick-v3';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  'https://cdn-icons-png.flaticon.com/512/3602/3602145.png'
+];
 
 self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
-  event.waitUntil(clients.claim());
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      );
+    })
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request).then((networkResponse) => {
+        return caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
+        });
+      });
+    }).catch(() => caches.match('/index.html'))
   );
 });
